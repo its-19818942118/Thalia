fe4629685cee2239e986bd492231fe09e905e559
Eduardo Monsalve <ed_ma.info@proton.me>
2025-01-26 15:10:05 -0500

    Add a Nix flake for static linking

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+   description = "A very simple flake for static compilation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            glibc.static
+          ];
+        };
+    };
+}
